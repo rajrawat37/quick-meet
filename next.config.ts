6e02cde9b24c7aa3ba769b8ca2adee5717b32434
rajrawat37 <rawatraj995@gmail.com>
2025-06-28 15:05:29 +0530

    chore: initialize project with ShadCN, config, and base layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@lib": require("path").resolve(__dirname, "lib"),
+      "@components": require("path").resolve(__dirname, "components"),
+      // Add more aliases if needed
+    };
+    return config;
+  },
 };
 
 export default nextConfig;

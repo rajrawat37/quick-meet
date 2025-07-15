e43ac9aebedb42d5493313c5d49641102ca696ba
rajrawat37 <rawatraj995@gmail.com>
2025-07-15 23:04:33 +0530

    feat: implement CallList and MeetingCard components with call management
    Major Components Added:
    - CallList component: Displays and manages different types of calls (upcoming, ended, recordings)
    - MeetingCard component: Shows individual meeting details with join/copy functionality
    - useGetCalls hook: Fetches and filters call data from Stream API

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -93,41 +93,46 @@ const MeetingTypeList = () => {
         className="bg-yellow-1"
         handleClick={() => router.push('/recordings')}
       />
-      {!callDetails ? <MeetingModal
-        isOpen={meetingState === 'isScheduleMeeting'}
-        onClose={() => setMeetingState(undefined)}
-        title="Start an InstantMeeting"
-        className="text-center"
-        buttonText="Schedule Meeting"
-        handleClick={createMeeting}
-      >
-        <div className="flex flex-col gap-2.5">
-          <label className="text-base font-normal leading-[22.4px] text-sky-2">
-            Add a description
-          </label>
-          <Textarea
-            className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
-            onChange={(e: any) =>
-              setValues({ ...values, description: e.target.value })
-            }
-          />
-        </div>
-        <div className="flex w-full flex-col gap-2.5">
-          <label className="text-base font-normal leading-[22.4px] text-sky-2">
-            Select Date and Time
-          </label>
-          <ReactDatePicker
-            selected={values.dateTime}
-            onChange={(date) => setValues({ ...values, dateTime: date! })}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            timeCaption="time"
-            dateFormat="MMMM d, yyyy h:mm aa"
-            className="w-full rounded bg-dark-3 p-2 focus:outline-none"
-          />
-        </div>
-      </MeetingModal> :
+
+      {/*    */}
+
+      {!callDetails ?
+        <MeetingModal
+          isOpen={meetingState === 'isScheduleMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title="Start an InstantMeeting"
+          className="text-center"
+          buttonText="Schedule Meeting"
+          handleClick={createMeeting}
+        >
+          <div className="flex flex-col gap-2.5">
+            <label className="text-base font-normal leading-[22.4px] text-sky-2">
+              Add a description
+            </label>
+            <Textarea
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e: any) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex w-full flex-col gap-2.5">
+            <label className="text-base font-normal leading-[22.4px] text-sky-2">
+              Select Date and Time
+            </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
+          </div>
+        </MeetingModal>
+        :
         <MeetingModal
           isOpen={meetingState === 'isScheduleMeeting'}
           onClose={() => setMeetingState(undefined)}
@@ -155,4 +160,3 @@ const MeetingTypeList = () => {
 }
 
 export default MeetingTypeList
- 
--- constants/index.ts
+++ constants/index.ts
@@ -18,12 +18,20 @@ const sidebarLinks = [
         label: "Recordings",
         route: "/recordings",
         imgURL: "/icons/recordings.svg",
-    },      
+    },
     {
         label: "Personal Room",
         route: "/personal-room",
         imgURL: "/icons/add-personal.svg",
-    },    
+    },
 ]
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+];
 
 export default sidebarLinks;
